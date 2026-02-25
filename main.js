8f3185f04953e7b0329eb7c1a6c10d652ef55584
isaac.ic.7745 <isaac.ic.7745@gmail.com>
2026-02-25 07:16:56 +0000

    Update project files

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-// Add JS here
+const lottoNumbersContainer = document.querySelector('.lotto-numbers');
+const generatorBtn = document.querySelector('#generator-btn');
+
+generatorBtn.addEventListener('click', () => {
+    lottoNumbersContainer.innerHTML = '';
+    const numbers = new Set();
+    while (numbers.size < 6) {
+        const randomNumber = Math.floor(Math.random() * 45) + 1;
+        numbers.add(randomNumber);
+    }
+
+    for (const number of numbers) {
+        const numberDiv = document.createElement('div');
+        numberDiv.classList.add('lotto-number');
+        numberDiv.textContent = number;
+        lottoNumbersContainer.appendChild(numberDiv);
+    }
+});
